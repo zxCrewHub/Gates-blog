9d92099f635533106bd9e067061b78e03561cbd1
nadezhdaverkhovykh <verhyshka.mail@mail.ru>
2024-10-20 12:22:17 +0300

    frist

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import NV from "./components/NV";
+
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
       Hello andrew
       <img src="/images/test/personal.png" alt="" />
+      <NV></NV>
     </h1>
   );
 }
