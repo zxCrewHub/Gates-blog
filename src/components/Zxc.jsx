c6e3c697b4f33cffd418cdb412560a4f3896daca
deadpooleveryday <anbashun95@gmail.com>
2024-10-20 12:23:02 +0300

    newZXC

--- src/components/Zxc.jsx
+++ src/components/Zxc.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 const Zxc = () => {
   return (
     <div class="bg-gray-800 p-4 rounded-lg">
-      <span class="text-gray-900 dark:text-white text-4xl font-bold tracking-wide shadow-lg">
-        1000-7
-      </span>
+      <span class="text-gray-900 dark:text-white text-4xl font-bold tracking-wide shadow-lg">1000-7</span>
     </div>
   );
 }
