b6aeacb8d6c427aec6ef22f9086fd7ce14bfb685
Shota1985 <sany_snow@mail.ru>
2024-10-20 12:24:18 +0300

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { Shtest } from "./components/Shtest";
+
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
       Hello andrew
       <img src="/images/test/personal.png" alt="" />
+      <Shtest></Shtest>
     </h1>
   );
 }
