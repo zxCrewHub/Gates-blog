ad2d73f1f65dd6f71a1dd17a7f79e14e8e0958e6
BigZetena <V.O.Sharipov@gmail.com>
2024-10-20 11:45:59 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+      <img src="/images/test/personal.png" alt="" />
+    </h1>
+  );
+}
