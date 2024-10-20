097ddc508a3f451d57a59fef7cbde1c8d7e2887f
deadpooleveryday <anbashun95@gmail.com>
2024-10-20 12:15:39 +0300

    add zxc

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Zxc from "./components/Zxc";
+
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
       Hello andrew
+      <Zxc />
       <img src="/images/test/personal.png" alt="" />
     </h1>
   );
