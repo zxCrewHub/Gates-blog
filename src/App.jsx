ef709b2acaff70d7299cf75ab758e81876d5ba7b
BigZetena <V.O.Sharipov@gmail.com>
2024-10-20 11:55:58 +0300

    otcat

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
-      Hello world! Андрей, ты чево тут забыл????
+      Hello world! 
       <img src="/images/test/personal.png" alt="" />
     </h1>
   );
