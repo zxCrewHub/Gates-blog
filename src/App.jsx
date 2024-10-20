e8267531ea629a2026dcfc94de467ea8692efcad
BigZetena <V.O.Sharipov@gmail.com>
2024-10-20 11:57:17 +0300

    sadfssfs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
-      Hello world! 
+      Hello 
       <img src="/images/test/personal.png" alt="" />
     </h1>
   );
