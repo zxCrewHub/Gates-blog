9af4e7c04ec173f999ac5515a82e9d30398c7e3a
BigZetena <V.O.Sharipov@gmail.com>
2024-10-20 11:52:49 +0300

    andrew check

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+      Hello world! Андрей, ты чево тут забыл????
       <img src="/images/test/personal.png" alt="" />
     </h1>
   );
